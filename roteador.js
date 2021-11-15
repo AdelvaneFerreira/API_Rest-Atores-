33bf60974a927fe80c19a63be0136fc8d21f952d
AdelvaneFerreira <adelvanefernandesfs@gmail.com>
2021-11-15 10:07:45 -0300

    Criado o roteador

---
+++ roteador.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const controle = require('./Controladores/atores')
+
+const roteador = express()
+
+//Consulta os elementos existentes
+roteador.get("/atores", controle.consultaAtores)
+
+//Consulta o elemento através do Id
+roteador.get('/atores/:idConsultado', controle.consultaID)
+
+//Adiciona um novo elemento na lista
+roteador.post('/atores', controle.criarNovo)
+
+//Patch edita um elemento da lista
+roteador.patch('/atores/:idConsultado', controle.editarAtor)
+
+//Put substitui um elemento da lista
+roteador.put("/atores/:idconsultado", controle.substituir)
+
+// Delete apaga um elemento da lista
+roteador.delete("/atores/:idconsultado", controle.excluir)
+
+module.exports = roteador
